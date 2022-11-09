ea7c6454c265b426ac2a1752c0e19ae116f6c337
rithvikshankar <rithviks@gmail.com>
2022-11-09 14:27:22 +0530

    Fixed AddRoom and EditRoom

--- frontend/src/components/AddRoom.js
+++ frontend/src/components/AddRoom.js
@@ -47,39 +47,42 @@ const AddRoom = () => {
             
                 <form className="flex flex-col justify-center place-items-center" onSubmit={(e)=>onsubmit(e)}>
                     
-                    <div className="flex flex-wrap -mx-3 mb-6">
-                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-patient">
+
+                    <div className="w-full md:w-[30rem] px-3 mb-6 md:mb-0">
+                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-patient">
                             Patient
-                            </label>
-                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 
-                            mb-3 leading-tight focus:outline-none focus:border-gray-400 focus:bg-white" id="grid-patient" type="text"
-                            name='patient'
-                            onChange={(e)=>onchange(e)}
-                            />
-                        </div>
-                        <div className="w-full md:w-1/2 px-3">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-room">
-                            Room
-                            </label>
-                            {/* <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 
-                            leading-tight focus:outline-none focus:bg-white focus:border-gray-400" id="grid-room" type="text" 
-                            name='room'
-                            onChange={(e)=>onchange(e)}/> */}
-                            <select className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 
-                            leading-tight focus:outline-none focus:bg-white focus:border-gray-400" id="grid-paid" type="text" 
-                            name='room'
-                            onChange={(e)=>onchange(e)}
-                            >
-                                {/* <option value="true">True</option>
-                                <option value="false">False</option> */}
-                                {rooms.map(item=>
-                                    <option value={item._id}>{item.block} {item.roomNo}</option>
-                                )}
-                            </select>
-                        </div> 
+                        </label>
+                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 
+                        rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:border-gray-400 focus:bg-white" id="grid-patient" type="text" 
+                        placeholder="22-09-2022"
+                        name='patient'
+                        onChange={(e)=>onchange(e)}
+                        /><br/>
                     </div>
 
+                    <div className="w-full md:w-[30rem] px-3 mb-6 md:mb-0">
+                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-room">
+                        Room
+                        </label>
+                        {/* <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 
+                        leading-tight focus:outline-none focus:bg-white focus:border-gray-400" id="grid-room" type="text" 
+                        name='room'
+                        onChange={(e)=>onchange(e)}/> */}
+                        <select className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 
+                        leading-tight focus:outline-none focus:bg-white focus:border-gray-400" id="grid-paid" type="text" 
+                        name='room'
+                        onChange={(e)=>onchange(e)}
+                        >
+                            {/* <option value="true">True</option>
+                            <option value="false">False</option> */}
+                            {rooms.map(item=>
+                                <option value={item._id}>{item.block} {item.roomNo}</option>
+                            )}
+                        </select>
+                    </div> 
+
+                    
+                    <br/>
                     <div className="w-full md:w-[30rem] px-3 mb-6 md:mb-0">
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-from-date">
                             From
